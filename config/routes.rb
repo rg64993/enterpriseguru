2bed6735f387e4586bb9cdfb2f1c180659d90be2
Raul <comp_r_g@yahoo.com>
2015-01-28 23:24:44 +0000

    adding nesting purchases

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  resources :purchases
-
+  
   resources :employees
 
   resources :companies do
@@ -15,8 +14,10 @@ Rails.application.routes.draw do
   get 'welcome/pricing'
   get 'welcome/features'
   
-  resources :invoices
-
+  resources :invoices do
+    resources :purchases, except: [:index], controller: 'invoices/purchases'
+  end
+  
   root to: 'welcome#index'
   
 end
